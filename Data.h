955768148a3e1b4c0fa0bcacc440addfd9ec7b83
frezzerovshik <43182862+frezzerovshik@users.noreply.github.com>
2019-02-23 19:47:27 +0300

    Add files via upload

---
+++ Data.h
@@ -0,0 +1,9 @@
+#pragma once
+
+typedef struct Node
+{
+    int Value;//Целочисленное значение
+    int Num;//Порядковый номер элемента
+    Node *Previous;//Указатель на предыдущий элемент
+
+}Node;
